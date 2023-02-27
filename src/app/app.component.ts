c6e91d5490a2d3f4d9f908bfc5373486cd37c3b2
Yassa Salib <yassa.salib@interfaceagency.com>
2023-02-27 16:40:32 +0100

    master #comment Update loade icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  apiResult: any;
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
+
   ngOnInit() {
-    setInterval(() => this.callApi(), 4000);
+    this.callApi();
   }
-  data = []
 
   callApi() {
-    this.http
-      .get('http://worldtimeapi.org/api/timezone/Europe/Amsterdam')
-      .subscribe((data) => {
-        this.data = data
-      });
+    setInterval(() => {
+      this.getData().subscribe(
+        (result) => {
+          this.apiResult = result;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }, 4000);
+  }
+
+  getData(): Observable<any> {
+    return this.http.get('http://worldtimeapi.org/api/timezone/Africa/Abidjan').pipe(
+      switchMap((result) => {
+        return of(result);
+      }),
+      catchError((error: any) => {
+        console.error(error);
+        return of(null);
+      })
+    );
   }
 }
